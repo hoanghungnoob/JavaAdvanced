5e57b2cca29fa91bca7f59cc62fbd8ad57185492
Hoàng <hoanghung20030@gmail.com>
2023-11-16 23:18:30 +0700

    add project

--- attr/Database.java
+++ attr/Database.java
@@ -3,5 +3,5 @@ package attr;
 public class Database {
 	public static final String HOST_URI = "jdbc:mysql://localhost:3306/f1";
 	public static final String USER = "root";
-	public static final String PASSWORD = "";
+	public static final String PASSWORD = "mysql";
 }
